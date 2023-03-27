fdc3457e9fb046aed531fc39580fe3c083762c27
yves <yves_callaert@hotmail.com>
2023-03-27 16:01:59 +0200

    Send comes through

--- kafka_cons_prod/src/producer.rs
+++ kafka_cons_prod/src/producer.rs
@@ -1,12 +1,11 @@
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use std::time::Duration;
-
+use rdkafka::message::{Header, OwnedHeaders};
 mod utils;
 
 async fn produce() {
   let (topic, config) = utils::get_kafka_config().unwrap();
   let producer: FutureProducer = config.create().unwrap();
-
   // This loop is non blocking: all messages will be sent one after the other, without waiting
   // for the results.
   let futures = (0..50)
@@ -17,11 +16,16 @@ async fn produce() {
               .send(
                   FutureRecord::to(&topic)
                       .payload("message")
-                      .key("key"),
+                      .key("key")
+                      .headers(OwnedHeaders::new().insert(Header {
+                        key: "header_key",
+                        value: Some("header_value"),
+                    })),
                   Duration::from_secs(10),
               )
               ;
-          print!("Inside the loop");
+              //producer.send(FutureRecord { topic: (), partition: (), payload: (), key: (), timestamp: (), headers: () }, queue_timeout)
+
           // This will be executed when the result is received.
           print!("Delivery status for message {} received", i);
           delivery_status
