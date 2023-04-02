5bbdea70000453e26b86a3b761f9bd551520f14d
yves <yves_callaert@hotmail.com>
2023-04-02 14:36:41 +0200

    Adding postgress

---
+++ kafka_cons_prod/src/postgres_utils.rs
@@ -0,0 +1,40 @@
+
+use tokio_postgres::{NoTls, Error};
+extern crate serde;
+extern crate serde_json;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Person {
+  id: i32,
+  name: String,
+  state: String,
+  country: String
+}
+
+pub async fn read_sql_data_person() -> Result<Vec<Person>, Box<dyn std::error::Error>> {
+  println!("Connection to the database");
+  let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres port=54320 dbname=postgres", NoTls).await?;
+  println!("Connection is a success");
+  let mut persons : Vec<Person> = Vec::new();
+
+  tokio::spawn(async move {
+    if let Err(e) = connection.await {
+        eprintln!("connection error: {}", e);
+    }
+});
+
+  for row in client.query("SELECT id,name,state,country FROM dataprocessor", &[]).await? {
+    println!("Found a record");
+    let mut person = Person {
+      id: row.get(0),
+      name: row.get(1),
+      state: row.get(2),
+      country: row.get(3),
+    };
+    persons.push(person);
+  }
+  Ok((persons))
+  
+}
--- kafka_cons_prod/src/producer.rs
+++ kafka_cons_prod/src/producer.rs
@@ -1,21 +1,29 @@
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use std::time::Duration;
 use rdkafka::message::{Header, OwnedHeaders};
+
+#[macro_use] extern crate serde_derive;
+use postgres_utils::read_sql_data_person;
 mod utils;
+mod postgres_utils;
 
 async fn produce() {
   let (topic, config) = utils::get_kafka_config().unwrap();
   let producer: FutureProducer = config.create().unwrap();
+  let persons = read_sql_data_person().await;
+  let mut person_array = persons.unwrap();
   // This loop is non blocking: all messages will be sent one after the other, without waiting
   // for the results.
-  let futures = (0..50)
+  let futures = (0..person_array.len()-1)
       .map(|i| {
           // The send operation on the topic returns a future, which will be
           // completed once the result or failure from Kafka is received.
+          let value = person_array.pop().unwrap();
+          let serialized = serde_json::to_string(&value).unwrap().clone();
           let delivery_status = producer
               .send(
                   FutureRecord::to(&topic)
-                      .payload("message")
+                      .payload("t")
                       .key("key")
                       .headers(OwnedHeaders::new().insert(Header {
                         key: "header_key",
@@ -27,7 +35,7 @@ async fn produce() {
               //producer.send(FutureRecord { topic: (), partition: (), payload: (), key: (), timestamp: (), headers: () }, queue_timeout)
 
           // This will be executed when the result is received.
-          print!("Delivery status for message {} received", i);
+          print!("Delivery status for message {} received", serialized);
           delivery_status
       })
       .collect::<Vec<_>>();
